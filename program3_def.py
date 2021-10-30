63381da623e2ec4b49e9ff748d9b13916b4966ae
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-10-30 23:46:48 +0800

    - added program 3

---
+++ program3_def.py
@@ -0,0 +1,11 @@
+import math
+
+def Apple_machine(): 
+    amount_money = float(input("Enter the amount of money you have: ₱"))
+    apple_price = float(input("How much will you pay for an apple? ₱"))
+    apple_stock = 10
+    Calculation= amount_money - (apple_stock * apple_price)
+    print(f"You can buy {apple_stock} apples and your change is ₱{Calculation:.2f}.")
+
+
+Apple_machine()    
